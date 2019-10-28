d0911f878fbe709a8e1c7a0e7419bd7ef2108be7
Jason Huels and Jacqueline Remmel <jason@>
2019-10-28 08:29:14 -0700

    Add Pet class and initial database migration.

---
+++ Pets/Migrations/20191028152831_Initial.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Pets.Migrations
+{
+    public partial class Initial : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Pets",
+                columns: table => new
+                {
+                    PetId = table.Column<int>(nullable: false)
+                        .Annotation("MySql:ValueGenerationStrategy", MySqlValueGenerationStrategy.IdentityColumn),
+                    Name = table.Column<string>(nullable: true),
+                    Species = table.Column<string>(nullable: true),
+                    Age = table.Column<int>(nullable: false),
+                    Gender = table.Column<string>(nullable: true),
+                    Description = table.Column<string>(nullable: true),
+                    FavoriteThings = table.Column<string>(nullable: true),
+                    LeastFavoriteThings = table.Column<string>(nullable: true),
+                    Owner = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Pets", x => x.PetId);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Pets");
+        }
+    }
+}
---
+++ Pets/Models/Pet.cs
@@ -0,0 +1,15 @@
+namespace EpicodusPets.Models
+{
+    public class Pet
+    {
+        public int PetId { get; set; }
+        public string Name { get; set; }
+        public string Species { get; set; }
+        public int Age { get; set; }
+        public string Gender { get; set; }
+        public string Description { get; set; }
+        public string FavoriteThings { get; set; }
+        public string LeastFavoriteThings { get; set; }
+        public string Owner { get; set; }
+    }
+}
